8ed2aa586da788675b3e448af0a39be99ac2a272
Jakub_S91 <fidelion.web@gmail.com>
2023-11-30 14:51:40 +0100

    MYP-50 - ...

--- src/main/java/com/example/demo/LoanCalculator.java
+++ src/main/java/com/example/demo/LoanCalculator.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 public class LoanCalculator {
     private static double calculateFixedInstallment(double loanAmount, double oprocentowanie,
                                           int liczbaRat) {
-        double qPerMonth = 1 + (oprocentowanie / 12);
-        return (loanAmount * oprocentowanie) / (12 * (1 + Math.pow(qPerMonth, -liczbaRat)));
+        double w = 1 + (oprocentowanie / 12);
+        return (loanAmount * oprocentowanie) / (12 * (1 + Math.pow(w, -liczbaRat)));
     }
 }
